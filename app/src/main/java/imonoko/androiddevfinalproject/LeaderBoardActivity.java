322e94027be2a2595d168df754e5a30eeabe7993
ImonokoI <robertchiu13@gmail.com>
2017-12-16 13:18:17 -0500

    Revamped views of leaderboard and Statistics activites

--- app/src/main/java/imonoko/androiddevfinalproject/LeaderBoardActivity.java
+++ app/src/main/java/imonoko/androiddevfinalproject/LeaderBoardActivity.java
@@ -1,17 +1,21 @@
 package imonoko.androiddevfinalproject;
+/**
+ * Created by Robert on 12/4/2017.
+ */
 
+import android.graphics.Color;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
-
 import java.util.ArrayList;
-import java.util.Objects;
 
 import static android.util.TypedValue.COMPLEX_UNIT_PX;
 
@@ -25,63 +29,92 @@ public class LeaderBoardActivity extends AppCompatActivity {
         StatsPage();
     }
     public void StatsPage( ) {
+        LinearLayout LR = new LinearLayout(this);
+        LR.setOrientation(LinearLayout.VERTICAL);
+
+        ScrollView scrollView = new ScrollView(this);
+        GridLayout grid = new GridLayout(this);
         ArrayList<Statistics> stats = DBM.selectAllStats();
         ArrayList<Account> Acclist = DBM.selectAllAcc();
-        LinearLayout LR = new LinearLayout(this);
+        TextView[][] accountStats = new TextView[stats.size()+1][4];
+        Point size = new Point();
+        getWindowManager().getDefaultDisplay().getSize(size);
+
+        int width = size.x;
+        int height = size.y;
+        grid.setRowCount(stats.size()+1);
+        grid.setColumnCount(4);
+
+        //Title of the
+        TextView title = new TextView(this);
+        title.setText("LEADERBOARD");
+        title.setTextColor(Color.WHITE);
+        title.setTextSize(50f);
+        LR.addView(title,width,ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        TextView[] label = new TextView[4];
+        //create label for each stat attribute for every column
+        for(int i=0;i<4;i++)
+        {
+            label[i]= new TextView( this );
+            label[i].setTextColor(Color.WHITE);
+            label[i].setTextSize(20.0f);
+            grid.addView(label[i], (int) (width * .25),
+                    ViewGroup.LayoutParams.WRAP_CONTENT);
+        }
+        label[0].setText("Name");
+        label[1].setText("Score");
+        label[2].setText("Wins");
+        label[3].setText("Losses");
 
         if( stats.size( ) > 0 ) {
-            // create ScrollView and GridLayout
-            ScrollView scrollView = new ScrollView(this);
-            GridLayout grid = new GridLayout(this);
-            grid.setRowCount(stats.size()+1);
-            grid.setColumnCount(4);
-            // create an array of stats
-            TextView[][] accountStats = new TextView[stats.size()][4];
-
-            // retrieve width of screen
-            Point size = new Point();
-            getWindowManager().getDefaultDisplay().getSize(size);
-            int width = size.x;
-            int i = 0;
-
-            for ( Statistics stat : stats
-                    ) {
-
-                //initialize textview for each element
+            int i=0;
+            for(Statistics stat: stats) {
                 for(int j=0;j<4;j++)
                 {
                     accountStats[i][j] = new TextView( this );
+                    accountStats[i][j].setTextColor(Color.WHITE);
                     accountStats[i][j].setTextSize(20.0f);
+                    grid.addView(accountStats[i][j], (int) (width * .25),
+                            ViewGroup.LayoutParams.WRAP_CONTENT);
                 }
-                    //create label for each stat attribute for every column
-                    accountStats[i][0].setText(" " + Acclist.get(i).getUserName());
-                    //stat.getID() is a placeholder until we get actual ids
-                    accountStats[i][1].setText(" " + stat.getScore());
-                    accountStats[i][2].setText(" " + stat.getWins());
-                    accountStats[i][3].setText(" " + stat.getLosses());
-
-                    // add the elements to grid
-                for(int j=0;j<4;j++) {
-                    grid.addView(accountStats[i][j], (int) (width * .15),
-                            ViewGroup.LayoutParams.MATCH_PARENT);
-                }
+                accountStats[i][0].setText("" +getInitials(Acclist.get(i).getUserName()));
+                accountStats[i][1].setText("" + stat.getScore());
+                accountStats[i][2].setText("" + stat.getWins());
+                accountStats[i][3].setText("" + stat.getLosses());
                 i++;
             }
-            accountStats[0][0].setText("name");
-            accountStats[0][1].setText("score");
-            accountStats[0][2].setText("wins");
-            accountStats[0][3].setText("losses");
+
+            LR.setBackgroundColor(Color.BLACK);
             scrollView.addView( grid );
             LR.addView(scrollView);
         }
         else
         {
             TextView noStats = new TextView(this);
-            noStats.setText("No Stats available\nMake sure you're connected to the internet");
+            noStats.setTextColor(Color.WHITE);
+            noStats.setText("\n\n\nNo Stats available\nMake sure you're connected to the internet");
             noStats.setGravity(Gravity.CENTER_HORIZONTAL);
             noStats.setTextSize(COMPLEX_UNIT_PX,70f);
             LR.addView(noStats);
         }
+
+        ButtonHandler bh = new ButtonHandler();
+        Button back = new Button(this);
+        back.setOnClickListener(bh);
+        back.setText("BACK");
+        back.setTextSize(20f);
+        back.setLayoutDirection(LR.getBottom());
+        LR.addView(back, (int)(width*.35),(int) (height*.1) );
+
         setContentView(LR);
+    }
+    private class ButtonHandler implements View.OnClickListener {
+        public void onClick(View v) {finish();}
+    }
+    public String getInitials(String string)
+    {
+        char[] charArr= string.toCharArray();
+        return charArr[0]+""+charArr[charArr.length-1];
     }
 }
--- app/src/main/java/imonoko/androiddevfinalproject/StatisticsActivity.java
+++ app/src/main/java/imonoko/androiddevfinalproject/StatisticsActivity.java
@@ -1,27 +1,51 @@
 package imonoko.androiddevfinalproject;
 
+import android.graphics.Color;
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-
+/**
+ * Created by Robert on 12/4/2017.
+ */
 public class StatisticsActivity extends AppCompatActivity {
     private LoginActivity LA = new LoginActivity();
+    private int currentID;
+    private DatabaseManager DBM;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        DatabaseManager DBM = new DatabaseManager(this);
-        RelativeLayout RL = new RelativeLayout(this);
-        TextView ViewPlayerStat= new TextView(this);
-        TextView PlayerNameLabel= new TextView(this);
-        int currentID=LA.getloginID();
+        Stats();
+    }
+    public void Stats()
+    {
+        DBM = new DatabaseManager(this);
+        LinearLayout RL = new LinearLayout(this);
+        RL.setOrientation(LinearLayout.VERTICAL);
+        Point size = new Point();
+        getWindowManager().getDefaultDisplay().getSize(size);
+        currentID=LA.getloginID();
+        int width = size.x;
+        int height = size.y;
+
         Statistics CurrentStat;
         String CurrentAcc;
         CurrentAcc = DBM.getUser(currentID);
         CurrentStat=DBM.searchForStat(currentID);
-        ViewPlayerStat.setTextSize(40f);
+
+        TextView ViewPlayerStat= new TextView(this);
+        ViewPlayerStat.setTextSize(45f);
+        ViewPlayerStat.setTextColor(Color.WHITE);
+
+        TextView PlayerNameLabel= new TextView(this);
         PlayerNameLabel.setTextSize(50f);
-        PlayerNameLabel.setText(CurrentAcc);
+        PlayerNameLabel.setTextColor(Color.WHITE);
+        PlayerNameLabel.setText(CurrentAcc+"'s stats");
+
         if(CurrentStat==null)
         {
             ViewPlayerStat.setText("No Stats on file");
@@ -29,11 +53,22 @@ public class StatisticsActivity extends AppCompatActivity {
         else
         {
             ViewPlayerStat.setText("\nUser ID: "+CurrentStat.getID()
-                    +"\nWins: "+CurrentStat.getWins()+"\nLosses: "+CurrentStat.getLosses()+"\nScore "+CurrentStat.getScore());
-
+                    +"\n\nWins: "+CurrentStat.getWins()+"\nLosses: "+CurrentStat.getLosses()+"\nScore: "+CurrentStat.getScore());
         }
+        ButtonHandler bh = new ButtonHandler();
+        Button back = new Button(this);
+        back.setOnClickListener(bh);
+        back.setText("BACK");
+        back.setTextSize(20f);
+        back.setLayoutDirection(RL.getBottom());
+        RL.addView(back, (int)(width*.35),(int) (height*.1) );
+        RL.setBackgroundColor(Color.BLACK);
+        RL.addView(PlayerNameLabel);
         RL.addView(ViewPlayerStat);
         setContentView(RL);
 
+    }
+    private class ButtonHandler implements View.OnClickListener {
+        public void onClick(View v) {finish();}
     }
 }
